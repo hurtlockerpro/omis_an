fdfe7439c88cef9fc3715d06e3ddbc2255cfcf6c
vahermihkel <vahermihkel@gmail.com>
2022-01-10 17:43:30 +0200

    class javascript

---
+++ news/js/main.js
@@ -0,0 +1,13 @@
+
+// 4a5de1e54b304bf2909af12bf979c242
+// https://newsapi.org/v2/everything?q=tesla&from=2021-12-10&sortBy=publishedAt&apiKey=4a5de1e54b304bf2909af12bf979c242
+
+
+class News {
+
+    summa(x, y){
+        return x + y
+    }
+}
+
+let n = new News()
