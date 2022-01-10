8f287c73aaef9952e8af594bea663224568ea0f4
vahermihkel <vahermihkel@gmail.com>
2022-01-10 20:13:05 +0200

    news v3

--- news/js/main.js
+++ news/js/main.js
@@ -43,6 +43,21 @@ class Url {
 
 class News extends Url {
 
+    #allNewsParsed
+
+
+    constructor(newSettings = undefined){
+        super()
+        // TODO
+        if (newSettings != undefined) this.settings = newSettings
+
+        this.newsKeyword = document.getElementById('newsKeyword')
+        this.newsCategory = document.getElementById('newsCategory')
+        this.btnNewsSearch = document.getElementById('btnNewsSearch')
+        this.btnNewsSearch.addEventListener('click', event => {
+            this.doSearch()
+        })
+    }
     /*
     color = ''
     #color2 = '' // private
@@ -61,7 +76,65 @@ class News extends Url {
     summa(x, y){
         return x + y
     }*/
+
+
+    async getData(){
+        let url = this.generateUrl()
+        let request = await fetch(url)
+        this.#allNewsParsed = await request.json();
+        console.log(this.#allNewsParsed)
+        if (this.#allNewsParsed.hasOwnProperty('status') && this.#allNewsParsed.status == 'ok') 
+        {
+            await this.fillSlides()
+        }
+    }
+
+    fillSlides(){
+        let slides = document.getElementsByClassName('carousel-item')
+
+        Array.from(slides).forEach((slide, index) => {
+            slide.querySelector('img').src = this.#allNewsParsed.articles[index].urlToImage
+            slide.querySelector('h5').innerText = this.#allNewsParsed.articles[index].title
+            slide.querySelector('p').innerText = this.#allNewsParsed.articles[index]
+                .description.substring(0, 30) + '...'
+        })
+    }
+
+    doSearch(){
+
+        console.log(
+            'select: ', this.newsCategory.value, 
+            ' text: ', this.newsKeyword.value
+        )
+        if (this.newsKeyword.value.trim().length > 3)
+        {
+            this.settings.q = this.newsKeyword.value
+            if (this.newsCategory.value != -1) this.settings.category = this.newsCategory.value
+
+            this.getData()
+
+            if (this.#allNewsParsed.status == 'error')
+            {
+            
+                let modalObj = document.getElementById('exampleModal')
+                modalObj.querySelector('#exampleModalLabel').innerText = this.#allNewsParsed.status
+                modalObj.querySelector('.modal-body').innerHTML = this.#allNewsParsed.message
+
+                var myModal = new bootstrap.Modal(modalObj)
+                myModal.show()
+            }
+
+            console.log(this.generateUrl())
+        }
+    }
+
+
+
 }
+
+let myNews = new News()
+myNews.getData()
+
 
 /*
 let n = new News('red') // new object -> polymorfizm
